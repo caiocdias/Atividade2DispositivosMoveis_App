1950e91430cac1a26c5bdef729b16e5a6702219f
Isabely Melo <isabelymelo09@gmail.com>
2026-03-30 21:29:36 -0300

    implemented logic for Mercadoria in CadastroMercadoriaActivity

--- app/src/main/java/com/uemg/atividade2dispositivosmoveis_app/CadastroMercadoriaActivity.java
+++ app/src/main/java/com/uemg/atividade2dispositivosmoveis_app/CadastroMercadoriaActivity.java
@@ -1,12 +1,18 @@
 package com.uemg.atividade2dispositivosmoveis_app;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.uemg.atividade2dispositivosmoveis_app.model.Mercadoria;
 
 public class CadastroMercadoriaActivity extends AppCompatActivity {
 
@@ -19,6 +25,23 @@ public class CadastroMercadoriaActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        EditText codigo = findViewById(R.id.inputCodigoMercadoria);
+        EditText nome = findViewById(R.id.inputNomeMercadoria);
+        EditText peso = findViewById(R.id.inputPeso);
+        Button btnsalvar = findViewById(R.id.btnOkMercadoria);
+
+        btnsalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int c = Integer.parseInt(codigo.getText().toString());
+                String n = nome.getText().toString();
+                double p = Double.parseDouble(peso.getText().toString());
+
+                Mercadoria m = new Mercadoria(c, n, p);
+                MainActivity.listaMercadorias.add(m);
+            }
         });
     }
 }
